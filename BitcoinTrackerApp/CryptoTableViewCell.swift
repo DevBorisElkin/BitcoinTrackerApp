427ef9a5adad9e2585d1e9ce75c64b233b9cda0d
DevBorisElkin <ea.foreverrr@gmail.com>
2022-04-18 23:13:43 +0300

    creating a new file

---
+++ BitcoinTrackerApp/CryptoTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CryptoTableViewCell.swift
+//  BitcoinTrackerApp
+//
+//  Created by macuser on 4/18/22.
+//
+
+import UIKit
+
+class CryptoTableViewCell: UITableViewCell {
+
+   static let identifier = "CryptoTableViewCell"
+
+}
